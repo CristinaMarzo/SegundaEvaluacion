31101da8fe10ef7972f7998dda445ba9a34f6f9c
Cristina Marzo <55531207+CristinaMarzo@users.noreply.github.com>
2019-11-28 09:48:37 +0100

    Add files via upload

---
+++ Bucles.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+int main (){
+    char salir;
+    int ntabla;
+    std::cout<<"Que tabla deseas que muestre?: ";
+    std::cin>>ntabla;
+    std::cout<<"**************************************************************";
+    std::cout<<"* TABLA DEL ", ntabla, "                                      *";
+    std::cin>>salir;
+}
