a697a2138e284f2146117b2f8cdcb530a29df539
Cristina Marzo <55531207+CristinaMarzo@users.noreply.github.com>
2020-02-04 11:53:28 +0100

    Add files via upload

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -15,17 +15,21 @@ si ambas estan bien colocadas, las dejo tal cual y si no, intercambio en sus cas
 #include<cris_func.h>
 #include<string.h>//libreria para strcopy
 
+using namespace std;
+
 int main(){
     char salir;
     char *palabras[4];//OJO: si me dejo el asteruisco, no me puede guardar el numero hexadecimal de la posicion
     int cont;
     char aux[20];
     int longitud = 0;
+    int veces;
+    char *paux;
     
     //Leo cuatro palabras:
     for (cont=0; cont<4; cont++){
-        std::cout<<"Introduce tu palabra: ";
-        std::cin>>aux;
+        cout<<"Introduce tu palabra: ";
+        cin>>aux;
         longitud = devuelve_longitud(aux);
         palabras[cont] = (char *)malloc((longitud+1)*(sizeof(char)));//reserva. que calcule los bits, porque no lo calcula el automaticamente
         //si te comes el +1 es un error aleatorio
@@ -35,14 +39,28 @@ int main(){
         //expera que los dos parametros sean las posiciones de memoria de la primera letra de cada una de las palabras
     }
     
-    for (cont=0; cont<4; cont++)
-    
-    std::cout<<"\nLISTA DE PALABRAS\n";
+    cout<<"\nLISTA DE PALABRAS\n";
     for (cont=0; cont<4; cont++){
         printf("%s\n", palabras[cont]);
     }
     
+    //ORDENAMOS POR EL METODO DE LA BURBUJA
+    //strcmp = string comparation
+    for (veces=1; veces<4; veces++){//para asegurarnos esta ordenado tiene que repetirse n - 1 veces
+        for (cont=0; cont<3; cont++){
+            if (strcmp(palabras[cont], palabras[cont+1])>0){ //intercambio a 3 bandas
+               paux = palabras[cont];
+               palabras[cont] = palabras[cont+1];
+               palabras[cont+1] = paux;
+            }
+        }
+    }
     
-    std::cin>>salir;    
+    cout<<"\nLISTA DE PALABRAS ORDENADAS\n";
+    for (cont=0; cont<4; cont++){
+        printf("\n%s", palabras[cont]);
+    }
+    
+    cin>>salir;    
 }
 
