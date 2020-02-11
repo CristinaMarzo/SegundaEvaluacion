ed8feaa4f812dd064be6ea004072d48d056d7de3
Cristina Marzo <55531207+CristinaMarzo@users.noreply.github.com>
2020-02-11 21:20:23 +0100

    Update Estructuras.cpp

--- Estructuras.cpp
+++ Estructuras.cpp
@@ -1,3 +1,8 @@
+#include<iostream>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
 /*GUARDAR INFORMACION COMPLEJA: sobre un mismo dato guardo diferentes tipos de variables (tipos)
 listas: arrays (no son suficientes)
 ESTRUCTURAS:
@@ -9,3 +14,32 @@ array de diferentes estilos
 los guarda seguidos
 el orden da igual
 puedes hacer los que quieras*/
+
+int main(){
+  struct Amigo{
+    int n_amigo;
+    float dinero;
+  };
+  char salir;
+  int cont;
+  Amigo misAmigos[3]; //Guardo memoria para un dato de tipo amigo (Declaras variable de tipo Amigo)
+  for (cont=0; cont<3; cont++){
+    std::cout<<"Dime tu numero ";
+    std::cin>>misAmigos[cont].n_amigo; //Principio de programacion orientada a objetos
+    std::cout<<"Dime cuanto dinero tienes: ";
+    std::cin>>misAmigos[cont].dinero;
+  }
+
+  //Recupero los datos de mi Amigo pepito
+  
+  for (cont=0; cont<3; cont++){
+    std::cout<<"\nYo tengo un amigo que se llama pepito";
+    std::cout<<"\nPepito es mi amigo numero: ";
+    std::cout<<misAmigos[cont].n_amigo;
+    std::cout<<"\ny tiene ";
+    std::cout<<misAmigos[cont].dinero;
+    std::cout<<" euros";
+  }
+
+  std::cin>>salir;
+}
