e3b5b32f32c73d1f02d4c5e60091a508a01d19d7
Cristina Marzo <55531207+CristinaMarzo@users.noreply.github.com>
2020-02-11 11:29:37 +0100

    Add files via upload

---
+++ Estructuras.cpp
@@ -0,0 +1,11 @@
+/*GUARDAR INFORMACION COMPLEJA: sobre un mismo dato guardo diferentes tipos de variables (tipos)
+listas: arrays (no son suficientes)
+ESTRUCTURAS:
+struct Amigo{
+       int n.amigo;
+       float dinero;
+}; --> las estrcuturas necesitan un punto y coma
+array de diferentes estilos
+los guarda seguidos
+el orden da igual
+puedes hacer los que quieras*/
